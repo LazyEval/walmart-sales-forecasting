4136d175df14bce660bb415dbc3e2078e558e0a1
Matteo Latinov <matteo_latinov@yanmar.com>
2023-12-21 05:14:32 +0100

    Refactor TrainingPipeline and its components to account for new data models

--- src/walmart_model/components/preprocessor.py
+++ src/walmart_model/components/preprocessor.py
@@ -2,7 +2,7 @@ import numpy as np
 from sklearn.preprocessing import OrdinalEncoder
 
 N_CAT_FEATURES = 5
-N_NUM_FEATURES = 1
+N_NUM_FEATURES = 2
 
 
 class Preprocessor:
@@ -12,19 +12,20 @@ class Preprocessor:
         self.encoder = encoder
 
     def preprocess_inputs(self, items, train):
-        n_samples = len([i.sales for j in items for i in j.sales_records])
-        cat_features = np.empty((n_samples, N_CAT_FEATURES), dtype=object)
+        n_samples = len([i.sales for j in items for i in j.item_history])
+        cat_features = np.empty((n_samples, N_CAT_FEATURES), dtype=str)
         num_features = np.empty((n_samples, N_NUM_FEATURES))
         i = 0
         for item in items:
             item_tokens = item.id.split("_")
-            for sales_record in item.sales_records:
+            for history in item.item_history:
                 cat_features[i, 0] = item_tokens[0] + "_" + item_tokens[1] + "_" + item_tokens[2]
                 cat_features[i, 1] = item_tokens[0] + "_" + item_tokens[1]
                 cat_features[i, 2] = item_tokens[0]
                 cat_features[i, 3] = item_tokens[3] + "_" + item_tokens[4]
                 cat_features[i, 4] = item_tokens[3]
-                num_features[i, 0] = sales_record.sales
+                num_features[i, 0] = history.sales_lag_1
+                num_features[i, 1] = history.sales_lag_7
                 i += 1
         if train:
             return np.hstack([self.encoder.fit_transform(cat_features), num_features])
