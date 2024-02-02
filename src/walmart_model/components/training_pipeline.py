ab5f8f7ae16040e784620022de717897608af493
Matteo Latinov <matteo_latinov@yanmar.com>
2024-02-02 18:54:58 +0100

    Update TrainingPipeline to use lightgbm API components

--- src/walmart_model/components/training_pipeline.py
+++ src/walmart_model/components/training_pipeline.py
@@ -2,16 +2,17 @@ from config import logger
 
 
 class TrainingPipeline:
-    def __init__(self, item_loader, item_splitter, model, scorer):
+    def __init__(self, item_loader, item_splitter, model_trainer, forecast_horizon, scorer):
         self.item_loader = item_loader
         self.item_splitter = item_splitter
-        self.model = model
+        self.model_trainer = model_trainer
+        self.forecast_horizon = forecast_horizon
         self.scorer = scorer
 
     def run(self):
         items = self.item_loader.get_items()
         train_items, valid_items = self.item_splitter.split_items(items)
-        self.model.train(train_items)
-        predictions = self.model.predict(valid_items)
+        model = self.model_trainer.get_model(train_items, valid_items, self.forecast_horizon)
+        predictions = model.predict(valid_items)
         rmsse = self.scorer.get_rmsse(train_items, valid_items, predictions)
-        logger.logger.info(f"Validaion RMSSE: {rmsse:.3f}\n{len(train_items)} items")
+        logger.logger.info(f"Validation RMSSE: {rmsse:.3f}\n{len(train_items)} items")
