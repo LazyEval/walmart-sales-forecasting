9a510af609b6ed7bd209db5d624de30e1c7e8dd0
Matteo Latinov <matteo_latinov@yanmar.com>
2024-03-02 05:51:14 +0100

    Move forecast_period from __init__ to method to allow flexible prediction

--- src/walmart_model/components/walmart_model.py
+++ src/walmart_model/components/walmart_model.py
@@ -4,12 +4,11 @@ from walmart_model.data_models.prediction import Forecast, Prediction
 
 
 class WalmartModel:
-    def __init__(self, preprocessor, predictor, forecast_horizon):
+    def __init__(self, preprocessor, predictor):
         self.preprocessor = preprocessor
         self.predictor = predictor
-        self.forecast_horizon = forecast_horizon
 
-    def predict(self, items):
+    def predict(self, items, forecast_period):
         inputs = self.preprocessor.preprocess_inputs(items=items, train=False)
         predictions = []
         for i, item in enumerate(items):
@@ -17,11 +16,11 @@ class WalmartModel:
                 Prediction(
                     item_id=item.id,
                     forecasts=[
-                        Forecast(date=j.date + timedelta(self.forecast_horizon), sales=k)
-                        for j, k in zip(
+                        Forecast(date=record.date + timedelta(forecast_period), sales=prediction)
+                        for record, prediction in zip(
                             item.records,
                             self.predictor.predict(inputs)[
-                                i * self.forecast_horizon : (i + 1) * self.forecast_horizon
+                                i * forecast_period : (i + 1) * forecast_period
                             ],
                         )
                     ],
