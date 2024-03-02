53586d95fc5d58888d00c4f6b24430f836927fe8
Matteo Latinov <matteo_latinov@yanmar.com>
2024-03-02 05:49:49 +0100

    Update ItemSplitter to split weekly for multi horizon forecasting

--- src/walmart_model/components/item_splitter.py
+++ src/walmart_model/components/item_splitter.py
@@ -2,13 +2,23 @@ from walmart_model.data_models.item import Item
 
 
 class ItemSplitter:
-    def __init__(self, forecast_horizon):
-        self.forecast_horizon = forecast_horizon
+    def __init__(self, max_forecast_horizon):
+        self.max_forecast_horizon = max_forecast_horizon
 
-    def split_items(self, items):
+    def split_items(self, items, forecast_horizon):
         train_items = []
         valid_items = []
         for item in items:
-            train_items.append(Item(id=item.id, records=item.records[: -self.forecast_horizon]))
-            valid_items.append(Item(id=item.id, records=item.records[-self.forecast_horizon :]))
+            last_record_idx = len(item.records)
+            train_items.append(Item(id=item.id, records=item.records[:-forecast_horizon]))
+            valid_items.append(
+                Item(
+                    id=item.id,
+                    records=item.records[
+                        last_record_idx
+                        - (self.max_forecast_horizon - forecast_horizon + 7) : last_record_idx
+                        - (self.max_forecast_horizon - forecast_horizon)
+                    ],
+                )
+            )
         return train_items, valid_items
